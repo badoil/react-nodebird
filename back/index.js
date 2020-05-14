027428e36a5ef80389c044c2af27ca3bb487e524
oil <oil@macui-MacBook.local>
2020-05-14 18:54:29 +0900

    backend index.js: app.get

--- back/index.js
+++ back/index.js
@@ -40,6 +40,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+    req.send('server is running on deployment!!');
+})
+
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
 app.use('/api/posts', postsAPIRouter);
